2ff57342c3f925d95091ce026363091a5c6ebcaa
Aaron Hill <aaron.hill@me.com>
2014-09-05 12:47:52 -0400

    week 2 materials

---
+++ week02/assignment2/changeName.js
@@ -0,0 +1,20 @@
+// Step One: on line 2 create a variable with the name "firstName" that stores a first name
+
+
+/* Step Two: on line 8 create a variable with the name "firstNameStem" that stores the 
+             stem of the first name, meaning the name without the first consonant sound 
+             (if it begins with a consonant). For example, the stem of "Roger" would 
+             be "oger" and the stem of "Anna" would be "anna" */
+             
+
+// Do not modify the code beneath this line
+window.onload = function() {
+
+    document.getElementsByClassName('firstName')[0].textContent = firstName;
+    document.getElementsByClassName('firstName')[1].textContent = firstName;
+    document.getElementsByClassName('firstName')[2].textContent = firstName;
+    document.getElementsByClassName('firstNameStem')[0].textContent = firstNameStem;
+    document.getElementsByClassName('firstNameStem')[1].textContent = firstNameStem;
+    document.getElementsByClassName('firstNameStem')[2].textContent = firstNameStem;
+
+}
---
+++ week02/inClassExercise/greeting.js
@@ -0,0 +1,10 @@
+var greeting = 'Heigho';
+
+// Do not modify the code beneath this line
+
+window.onload = function() {
+
+    document.getElementsByClassName('greeting')[0].textContent = greeting;
+    document.getElementsByClassName('greeting')[1].textContent = greeting;
+
+}
